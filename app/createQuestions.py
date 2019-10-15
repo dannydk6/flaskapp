359b6befdc438c5ccabd37d16fd48a7db7ee79bf
Daniel Alarcon <dannydk6@neogenesis.local>
2019-10-15 18:13:24 -0400

    auto-update

---
+++ app/createQuestions.py
@@ -0,0 +1,12 @@
+from app import app, db
+from app.models import User, Question
+
+
+def main():
+    q1 = Question(qnumber=1, title='Question 1',
+                  description='How many unique cities have games been played in?',
+                  answer=1965,
+                  base_score=1)
+
+    db.session.add(q1)
+    db.session.commit()
