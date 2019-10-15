0852f674b70c1cf73ac712ee09893179545ca302
Daniel Alarcon <dannydk6@neogenesis.local>
2019-10-15 19:12:06 -0400

    auto-update

--- app/createQuestions.py
+++ app/createQuestions.py
@@ -6,7 +6,7 @@ def main():
     q1 = Question(qnumber=1, title='Question 1',
                   description='How many unique cities have games been played in?',
                   answer=1965,
-                  base_score=1)
+                  base_score=10)
 
     db.session.add(q1)
     db.session.commit()
@@ -15,8 +15,8 @@ def main():
 def q2():
     q1 = Question(qnumber=2, title='Question 2',
                   description='How many games were played in Glasgow, Scotland?',
-                  answer=,
-                  base_score=1)
+                  answer=317,
+                  base_score=10)
 
     db.session.add(q1)
     db.session.commit()
